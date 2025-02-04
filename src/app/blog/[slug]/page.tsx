7f73fec3fa93a59f9f6081ca8535fae238084492
FIRST_NAME LAST_NAME <shabazbazigar@gmail.com>
2025-02-04 19:43:33 +0530

    fix params types

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,11 +1,5 @@
 import BlogPost from '@/components/BlogPostComponents/BlogPost';
-import  PageProps  from 'next';  // Import Next.js PageProps type
 
 export default function Page({ params }: { params: { slug: string } }) {
-  // Ensure `params` is correctly destructured and accessed
-  if (!params || !params.slug) {
-    return <div>Error: Slug not found</div>;
-  }
-
   return <BlogPost slug={params.slug} />;
 }
