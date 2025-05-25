779e3659f21e04c5da5f35bb06a0d7a449db6815
FIRST_NAME LAST_NAME <shabazbazigar@gmail.com>
2025-05-25 14:30:20 +0530

    commit

--- src/app/payment/success/page.tsx
+++ src/app/payment/success/page.tsx
@@ -1,5 +1,7 @@
-"use client";
+export const dynamic = "force-dynamic";
 
+"use client";
+ 
 import React from "react";
 import { useSearchParams, useRouter } from "next/navigation";
 import { useSession } from "next-auth/react";
